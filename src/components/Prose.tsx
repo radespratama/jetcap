2c23870948b56e175eb9644c5cd84d14291e102d
radespratama <puturades89@gmail.com>
2022-07-30 22:38:40 +0800

    update xss in component

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { marked } from "marked";
 import hljs from "highlight.js";
+import xss from "xss";
 
 interface Props {
   data: string;
@@ -17,7 +18,7 @@ export default function Prose({ data }: Props) {
     <>
       <div
         className="prose prose-invert max-w-none prose-pre:font-medium prose-a:text-sky-400 prose-a:font-medium prose-a:shadow-undersky prose-a:no-underline"
-        dangerouslySetInnerHTML={{ __html: marked(data) }}
+        dangerouslySetInnerHTML={{ __html: xss(marked(data)) }}
       />
     </>
   );
